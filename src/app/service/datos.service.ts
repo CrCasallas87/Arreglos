3efa539cc2f734f1116e5856ee6cba5ffdfd7779
CrCasallas87 <crcasallas@gmail.com>
2022-02-07 15:14:12 -0500

    003 - Orden del Menu

--- src/app/service/datos.service.ts
+++ src/app/service/datos.service.ts
@@ -11,16 +11,16 @@ export class DatosService {
 
   public menu: any = [
     {
-      titulo: 'starwars',
-      icono: 'metismenu-icon pe-7s-rocket',
-      subtitulo: 'Datos Starwars',
-      url: 'starwars'
-    },
-    {
       titulo: 'Nasa',
       icono: 'metismenu-icon pe-7s-star',
       subtitulo: 'Noticia',
       url: '/nasa'
+    },
+    {
+      titulo: 'starwars',
+      icono: 'metismenu-icon pe-7s-rocket',
+      subtitulo: 'Datos Starwars',
+      url: 'starwars'
     }
   ];
   //https://api.nasa.gov/planetary/apod?api_key=pxkLb9kTM4jRA6HG7nGZaxfh0MDamYzB60MD4jET
